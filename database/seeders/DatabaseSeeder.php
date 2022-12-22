c903c3d48da09966bc319b53a3ae9acf1b0d856d
aidacampean <aida.campean@yahoo.com>
2022-12-22 22:46:42 +0000

    Not needed as we insert radio station data in the migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,5 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         \App\Models\User::factory(10)->create();
-        \App\Models\RadioStation::factory(4)->create();
-
     }
 }
